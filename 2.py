3d0c8d9273e1f71251958493c72782e2417414ed
Robin <klokki2004@gmail.com>
2022-01-06 10:10:48 +0100

    enkel oppdatering

---
+++ 2.py
@@ -0,0 +1,42 @@
+print('Hello world')
+
+x = 'Hvordan har du det?'
+y = 10
+z = 'Bra'
+a = x + z
+b = True
+lis = []
+
+elever = ['Haidas', 'Erik', 'Bendik']
+larer = ['Tor', 'Johannes', 'Mats']
+
+while b:
+    # print(y)
+    y = y + 1
+    lis.append(y)
+    if y > 100:
+        # break
+        b = False
+lis[5] = 'hest'
+
+elevlarer = []
+
+elevlarer.append(elever)
+elevlarer.append(larer)
+
+print(elevlarer)
+
+print(len(lis))
+
+
+print(lis)
+
+
+
+print(type(y))
+print(type(x))
+print(type(b))
+
+print(x)
+print(a)
+
