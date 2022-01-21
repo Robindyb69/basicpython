4f786f95833c1729edc85787195544cd5bcff0fc
Robin <klokki2004@gmail.com>
2022-01-21 09:03:35 +0100

    enkel oppdatering

--- 1.py
+++ 1.py
@@ -1,3 +1,26 @@
-# Hei
-# Hallo
-#v cfx¨¨bvsd
+import pygame
+import random
+
+# Intialisere moduleene i pygame
+pygame.init()
+
+SCREEN = pygame.display.set_mode((500, 750))  # Setting the display
+
+# background
+BACKGROUND_IMAGE = pygame.image.load('background.jpg')
+
+#  BIRD
+BIRD_IMAGE = pygame.image.load('bird1.png')
+bird_x = 50
+bird_y = 300
+bird_y_change = 0
+
+def display_bird(x, y):
+    SCREEN.blit(BIRD_IMAGE, (x, y))
+
+# OBSTACLES
+OBSTACLE_WIDTH = 70
+OBSTACLE_HEIGHT = random.randint(150,450)
+OBSTACLE_COLOR = (211, 253, 117)
+OBSTACE_X_CHANGE = -4
+obstacle_x = 500
