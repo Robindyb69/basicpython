f61624cdacab2bafb9787c6c0cc58589d98c5be3
Robin <klokki2004@gmail.com>
2022-01-27 08:22:34 +0100

    fijdajnf

--- 1.py
+++ 1.py
@@ -9,18 +9,25 @@ screen_height = 936
 screen = Pygame.dislay.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Flappy Bird')
 
+#game variabler
+scroll_scroll = 0
+scroll_speed = 4
 
 #load images
 bg = pygame.image.load('img/bg.png')
+ground_img  = pygame.image.load('img/ground.png')
 
 run = True
 while run:
 
-    screen.blitt(bg, (0,0))
+    screen.blit(bg, (0,0))
+
+    screen.blit(ground_img, (ground_scroll, 768))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
 
     pygame.display.update()
 
